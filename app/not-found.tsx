9f02b2d1c851f5b17c0cc5254bd2360c33186615
Chris <christopherperrault@hotmail.com>
2024-03-19 16:18:20 -0400

    error page bug: changed filename to not-found, works now

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -15,7 +15,7 @@ const Error: React.FC<ErrorProps> = ({ error, reset }) => {
   return (
     <div className="mt-10 text-center">
       <h1>Something went wrong</h1>
-      <button className="hover:text-amber-600" onClick={() => reset()}>
+      <button className="hover:text-blue-600" onClick={() => reset()}>
         Try Again
       </button>
     </div>
