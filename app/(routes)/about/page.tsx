4464b6e87fd17cd1e54a4424bca8ab29664405b5
Chris <christopherperrault@hotmail.com>
2024-03-19 15:29:03 -0400

    added About page

---
+++ app/(routes)/about/page.tsx
@@ -0,0 +1,35 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl p-4 mx-auto space-y-4">
+      <h1 className="text-2xl font-medium text-blue-500"> About</h1>
+
+      <p>
+        Bacon ipsum dolor amet alcatra t-bone flank sirloin rump, short loin
+        pork loin buffalo ham hock chicken. Hamburger short loin flank kevin
+        rump swine burgdoggen ground round chicken shoulder ham bresaola. Short
+        loin rump jerky tongue. Burgdoggen tri-tip cupim beef ribs, strip steak
+        bacon hamburger chicken ham landjaeger cow meatloaf. Bacon jowl shankle
+        turducken andouille drumstick meatball, kielbasa strip steak pork chop
+        t-bone spare ribs salami ribeye.
+      </p>
+      <p>
+        Bacon ipsum dolor amet alcatra t-bone flank sirloin rump, short loin
+        pork loin buffalo ham hock chicken. Hamburger short loin flank kevin
+        rump swine burgdoggen ground round chicken shoulder ham bresaola. Short
+        loin rump jerky tongue. Burgdoggen tri-tip cupim beef ribs, strip steak
+        bacon hamburger chicken ham landjaeger cow meatloaf. Bacon jowl shankle
+        turducken andouille drumstick meatball, kielbasa strip steak pork chop
+        t-bone spare ribs salami ribeye.
+      </p>
+      <p>
+        Bacon ipsum dolor amet alcatra t-bone flank sirloin rump, short loin
+        pork loin buffalo ham hock chicken. Hamburger short loin flank kevin
+        rump swine burgdoggen ground round chicken shoulder ham bresaola. Short
+        loin rump jerky tongue. Burgdoggen tri-tip cupim beef ribs, strip steak
+        bacon hamburger chicken ham landjaeger cow meatloaf. Bacon jowl shankle
+        turducken andouille drumstick meatball, kielbasa strip steak pork chop
+        t-bone spare ribs salami ribeye.
+      </p>
+    </div>
+  );
+}
