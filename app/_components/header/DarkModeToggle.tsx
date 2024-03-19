c10191f7237fd1f53a6153407263549c652a28d7
Chris <christopherperrault@hotmail.com>
2024-03-19 14:57:32 -0400

    added next-themes, dark mode toggle

---
+++ app/_components/header/DarkModeToggle.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { MdLightMode } from "react-icons/md";
+import { BsFillMoonFill } from "react-icons/bs";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function DarkModeToggle() {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  // used to bypass possible hydration error where the user comes onto the page with system settings, changes the theme and tries to naviagte to another page
+  useEffect(() => setMounted(true), []);
+
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  return (
+    <>
+      {mounted &&
+        (currentTheme === "dark" ? (
+          <MdLightMode
+            className="text-xl cursor-pointer hover:text-amber-500"
+            onClick={() => setTheme("light")}
+          />
+        ) : (
+          <BsFillMoonFill
+            className="text-xl cursor-pointer hover:text-amber-500"
+            onClick={() => setTheme("dark")}
+          />
+        ))}
+    </>
+  );
+}
+
+// import { HiOutlineLightBulb } from "react-icons/hi2";
--- app/_components/header/Header.tsx
+++ app/_components/header/Header.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { FiHome } from "react-icons/fi";
 import { AiOutlineInfoCircle } from "react-icons/ai";
 import TopNavItem from "./TopNavItem";
+import DarkModeToggle from "./DarkModeToggle";
 
 export default function Header() {
   return (
@@ -11,6 +12,8 @@ export default function Header() {
           <TopNavItem title="HOME" url="/" Icon={FiHome} />
           <TopNavItem title="ABOUT" url="/about" Icon={AiOutlineInfoCircle} />
         </div>
+
+        <DarkModeToggle />
         <Link href="/">
           <h2>
             <span className="px-2 py-1 mr-1 font-bold rounded-lg bg-blue-500">
